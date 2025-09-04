771cffd1f3ca6e3aeab29550d4e197bf3538edd3
git <56557352+yannicklevederpvtl@users.noreply.github.com>
2025-09-04 10:51:04 -0400

    Add jupyter server

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,4 +1,8 @@
+import os
+
 c = get_config()
+
+# Basic JupyterLab settings
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = int(os.environ.get('PORT', 8888))
 c.NotebookApp.open_browser = False
@@ -6,3 +10,12 @@ c.NotebookApp.allow_root = True
 c.NotebookApp.token = ''
 c.NotebookApp.password = ''
 c.NotebookApp.allow_origin = '*'
+
+# Basic collaboration settings - Let the extension handle these automatically
+c.ServerApp.disable_check_xsrf = True
+c.ServerApp.allow_remote_access = True
+
+# JupyterLab specific collaboration settings
+c.ServerApp.jpserver_extensions = {
+    'jupyter_server_ydoc': True,
+}
